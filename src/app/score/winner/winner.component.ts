3a0dbe3aa0a70985b1500f2e2e499f156eecc894
= <=>
2023-07-18 14:31:23 +0200

    update

--- src/app/score/winner/winner.component.ts
+++ src/app/score/winner/winner.component.ts
@@ -6,5 +6,5 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./winner.component.scss'],
 })
 export class WinnerComponent {
-  @Input() winner!: string;
+  @Input() player!: string;
 }
