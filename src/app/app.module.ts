79055747b65e337cd3dddbf0e8b051c922c3239e
Robert Strzelczyk <123934951+rStrzelczyk98@users.noreply.github.com>
2023-07-18 14:34:19 +0200

    btn group (#7)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScoreComponent } from './score/score.component';
 import { WinnerComponent } from './score/winner/winner.component';
+import { ButtonGroupComponent } from './button-group/button-group.component';
 
 @NgModule({
-  declarations: [AppComponent, ScoreComponent, WinnerComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent, ScoreComponent, WinnerComponent,
+    ButtonGroupComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
